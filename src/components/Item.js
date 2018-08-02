ad949d148a693bb1d2cf759e4cb04d321d60782a
Andy Smith <asmith@AFA.private>
2018-08-02 13:49:49 +1000

    removed priority number from item componenet

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,8 +5,9 @@ const Item = (props) => (
   <div>
     <div className={`item__wrapper item__priority--${props.priority}`} onDoubleClick={(e) => {props.handleEditItem(props)}}>
       <div className="item__name">{props.itemText}</div>
-      <div className="item__priority">{props.priority}</div>
+      {/* <div className="item__priority">{props.priority}</div> */}
       {/* <button className="btn item__edit" >Edit</button> */}
+      <button className="btn btn-success item__remove" onClick={(e) => {props.handleRemoveItem(props)}}>{'\u2714'}</button>
       <button className="btn btn-red item__remove" onClick={(e) => {props.handleRemoveItem(props)}}>{'\u2718'}</button>
     </div>
   </div>
