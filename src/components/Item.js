492b493338c9ae2648287f4e407c9c94b76a1db6
Andy Smith <asmith@AFA.private>
2018-08-03 09:29:08 +1000

    changed yellow color for ranie

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Item = (props) => (
-  <div>
+
     <div className={`item__wrapper item__priority--${props.priority}`} onDoubleClick={(e) => {props.handleEditItem(props)}}>
       <div className="item__name">{props.text}</div>
       {/* <div className="item__priority">{props.priority}</div> */}
@@ -10,7 +10,6 @@ const Item = (props) => (
       <button className="btn btn-success item__remove" onClick={(e) => {props.handleRemoveItem(props)}}>{'\u2714'}</button>
       <button className="btn btn-red item__remove" onClick={(e) => {props.handleRemoveItem(props)}}>{'\u2718'}</button>
     </div>
-  </div>
 );
 
 
