663c9a277a5b1f5c4e95084432521330bd840f46
Andy Smith <asmith@AFA.private>
2018-09-20 13:39:24 +1000

    moved the complete item button to topline for better UX

--- src/components/Item.js
+++ src/components/Item.js
@@ -66,6 +66,22 @@ class Item extends Component {
       >
         {!this.props.editable && (
           <div className={`item__topline`} onClick={handleExpand} id={this.props.id}>
+            {!this.props.editable &&
+              (this.props.handleCompleteItem && (
+                <button
+                  className="btn btn-circle btn-green item__complete"
+                  onClick={e => {
+                    this.props.handleCompleteItem(this.props);
+                  }}
+                >
+                  <svg viewBox="0 0 512 512">
+                    <path
+                      fill="currentColor"
+                      d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
+                    />
+                  </svg>
+                </button>
+              ))}
             <div className="item__name">{this.props.text}</div>
             {this.props.editable && this.props.dueDate ? (
               <div />
@@ -137,22 +153,7 @@ class Item extends Component {
                 </button>
               ))}
 
-            {!this.props.editable &&
-              (this.props.handleCompleteItem && (
-                <button
-                  className="btn btn-circle btn-green item__complete"
-                  onClick={e => {
-                    this.props.handleCompleteItem(this.props);
-                  }}
-                >
-                  <svg viewBox="0 0 512 512">
-                    <path
-                      fill="currentColor"
-                      d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-                    />
-                  </svg>
-                </button>
-              ))}
+            
 
             {!this.props.editable && (
               <button
